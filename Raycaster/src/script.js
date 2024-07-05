cb36c0159e25cd64e52cfe9f295451ced9a72df0
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-07-06 02:11:44 +0530

    ray origin and direction

--- Raycaster/src/script.js
+++ Raycaster/src/script.js
@@ -19,23 +19,37 @@ const scene = new THREE.Scene()
  * Objects
  */
 const object1 = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.SphereGeometry(0.5, 16, 16),
     new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 object1.position.x = - 2
 
 const object2 = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.SphereGeometry(0.5, 16, 16),
     new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 
 const object3 = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.SphereGeometry(0.5, 16, 16),
     new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
+
+
+// Raycaster
+const raycaster = new THREE.Raycaster()
+const rayOrigin = new THREE.Vector3(-3, 0, 0)
+const rayDirection = new THREE.Vector3(10,0,0)
+rayDirection.normalize()
+raycaster.set(rayOrigin, rayDirection)
+
+const intersect = raycaster.intersectObject(object2)
+console.log(intersect)
+
+const intersects = raycaster.intersectObjects([object1, object2, object3])
+console.log(intersects)
 
 /**
  * Sizes
@@ -89,6 +103,22 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5 
+    object2.position.y = Math.sin(elapsedTime * 0.4) * 1.5 
+    object3.position.y = Math.sin(elapsedTime * 0.5) * 1.5 
+
+
+    //cast a ray
+    const rayOrigin = new THREE.Vector3(-3,0,0)
+    const rayDirection = new THREE.Vector3(1,0,0)
+    rayDirection.normalize()
+
+    raycaster.set(rayOrigin, rayDirection)
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+    console.log(intersects)
 
     // Update controls
     controls.update()
