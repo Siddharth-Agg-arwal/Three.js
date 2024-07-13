1ee9dc1ec54e9c0070a1ec0fbc97c05d475427aa
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-07-14 04:07:29 +0530

    antialiasing, bias, sRGBencoding, tonemapping

--- 23-realistic-render/src/script.js
+++ 23-realistic-render/src/script.js
@@ -2,12 +2,56 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+// import { debug } from 'webpack'
+
+// Loaders
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+gltfLoader.setDRACOLoader(dracoLoader)
+
+
+
+// models
+gltfLoader.load(
+    '/models/oldMan-MonsterCompressed.glb',
+    (gltf) => {
+        scene.add(gltf.scene)
+        // gltf.scene.position.set(0,-4,0)
+        // gltf.scene.rotation.y = Math.PI * 0.8
+        // gltf.scene.scale.set(10,10,10)
+
+        // gui.add(gltf.scene.rotation, 'y').min(-Math.PI).max(Math.PI).step(0.001).name('rotation')
+        // updateAllMaterials()
+    }
+)
+
+// update all materials
+const updateAllMaterials = () => 
+{
+    scene.traverse((child) => {
+        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial){
+            child.material.envMap = environmentMap
+            child.material.envMapIntensity = debugObject.envMapIntensity
+            child.castShadow = true
+            child.receiveShadow = true
+        }
+    })
+}
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+const debugObject = {}
+
+debugObject.envMapIntensity = 5
+gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(0.001).onChange(updateAllMaterials)
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -15,10 +59,39 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+
+// env maps
+const environmentMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg',
+])
+
+environmentMap.colorSpace = THREE.SRGBColorSpace
+// environmentMap.toneMapping = THREE.ACESFilmicToneMapping
+
+//add the env map to the scene
+scene.background = environmentMap
+
+//applies env map to all materials (which support the map), however we need to update the env map to each child using custom function if we want to be able to tweak it
+// scene.environment = environmentMap
+
 // Lights
 const directionalLight = new THREE.DirectionalLight('#ffffff',1)
 directionalLight.position.set(0.25, 3, -2.25)
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024,1024)
+
+//this prevents the smooth surfaces from casting shadow on their own surface causing shadow acne
+directionalLight.shadow.normalBias = 0.01
 scene.add(directionalLight)
+
+const directionLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionLightCameraHelper)
 
 gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
 gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('lightX')
@@ -29,11 +102,11 @@ gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('lightZ'
 /**
  * Test sphere
  */
-const testSphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial()
-)
-scene.add(testSphere)
+// const testSphere = new THREE.Mesh(
+//     new THREE.SphereGeometry(1, 32, 32),
+//     new THREE.MeshStandardMaterial()
+// )
+// scene.add(testSphere)
 
 /**
  * Sizes
@@ -74,11 +147,35 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias:true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// renderer.outputEncoding = THREE.sRGBEncoding
+// THREE.ColorManagement.legacyMode = false
+renderer.outputColorSpace = THREE.SRGBColorSpace
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 1
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+
+gui.add(renderer, 'toneMapping', {
+    No: THREE.NoToneMapping,
+    Linear:THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmicToneMapping: THREE.ACESFilmicToneMapping
+}).onFinishChange(() =>{
+    renderer.toneMapping = Number(renderer.toneMapping)
+    updateAllMaterials()
+})
+
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001)
 // renderer.physicallyCorrectLights = true
+// renderer.useLegacyLights = false
+
 
 // renderer.physically
 
