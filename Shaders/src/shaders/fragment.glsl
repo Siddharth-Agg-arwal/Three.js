3660de8a83c8eaf085a5929f54cdc12b349b681b
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-07-16 15:55:09 +0530

    using uniforms to add custom textures

--- Shaders/src/shaders/fragment.glsl
+++ Shaders/src/shaders/fragment.glsl
@@ -1,8 +1,20 @@
 precision mediump float;
 // varying float vRandom;
 
+//to fetch color 
 uniform vec3 uColor;
 
+//to fetch texture
+uniform sampler2D uTexture;
+
+varying float vElevation;
+
+//uv coordinates from vertex shader
+varying vec2 vUv;
+
 void main(){
-    gl_FragColor = vec4(uColor, 1.0);
+    vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 2.0 + 0.6;
+    // gl_FragColor = vec4(uColor, 1.0);
+    gl_FragColor = textureColor;
 }
--- Shaders/src/shaders/vertex.glsl
+++ Shaders/src/shaders/vertex.glsl
@@ -8,6 +8,11 @@ uniform float uTime;
 // uniform mat4 modelViewMatrix; combined matrix for both viewMatrix and ModelMatrix
 
 attribute vec3 position;
+
+attribute vec2 uv;
+varying vec2 vUv;
+
+varying float vElevation;
 // attribute float aRandom;
 
 // varying float vRandom;
@@ -16,14 +21,24 @@ void main(){
     
     //break down into each multiple components of theh gl_Position gives us better control over each component
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-    modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation  += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+
+    modelPosition.z += elevation;
+    // modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
+    // modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
     // modelPosition.z += aRandom * 0.01;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
+
+    vUv = uv;
+
+    vElevation = elevation;
 
     // vRandom = aRandom;
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
