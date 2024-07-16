9f194e01512bd8be3ff6f9ebe159793aafed6dc0
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-07-16 15:05:10 +0530

    vertex shader

--- Shaders/src/shaders/vertex.glsl
+++ Shaders/src/shaders/vertex.glsl
@@ -3,9 +3,19 @@ precision mediump float;
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+// uniform mat4 modelViewMatrix; combined matrix for both viewMatrix and ModelMatrix
 
 attribute vec3 position;
 
 void main(){
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    
+    //break down into each multiple components of theh gl_Position gives us better control over each component
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x * 20.0) * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+    // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
